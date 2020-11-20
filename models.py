85ecfc045d66d6b819d40c23f80ccc78354b7591
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-20 13:55:09 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,6 @@
+from app import db
+
+class Servlet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Integer(4))
+    latest_ver = db.Column(db.String(4), index=True, unique=True)
