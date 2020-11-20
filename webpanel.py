6d447236555fb4e65ef330a650e925f1a0390728
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-11-20 13:50:32 -0500

    Update webpanel.py

--- webpanel.py
+++ webpanel.py
@@ -1,19 +1,31 @@
 print('PLEASE CLEAN webpanel.py')
-from cam import app
+from cam import app, db
 from models import Servlet, FileServlet # Non-existent at the time of writing this
-
+from wtforms import *
+from wtforms.validators import *
 class StatusForm(Form):
     choices = ['OK','Maintaince']
-    status = SelectField('Status', choices = choices, validators = [Required()])
+    status = SelectField('Status', choices = choices, validators = [DataRequired()])
 app.config['SECRET_KEY'] = 'changemii'
 lock_debug = True # Do not set to False until further notice
 if lock_debug:
      print('[ERROR] webpanel is imported, but is locked!')
      exit()
-@app.route('/wp/status')
+@app.route('/wp/status',methods=['GET','POST'])
 def status():
     form = StatusForm()
-    return render_template('status.html')
+    status_codes = {'OK':1000,'Maintaince':1040}
+    if form.validate_on_submit():
+        # Get Servlet
+        servlet = Servlet.query.first()
+        # Change servlet status code
+        servlet.status_code = status_codes[form.status.data]
+        # Track servlet
+        db.session.add(servlet)
+        # Commit tracked data
+        db.session.commit()
+        
+    return render_template('status.html',form=form)
 
 
     
