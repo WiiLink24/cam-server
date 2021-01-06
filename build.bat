d8947c13b17b77fcb321093dda5044854d82a33d
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-01-05 19:22:09 -0500

    Why did I put this in here

--- build.bat
+++
@@ -1,8 +0,0 @@
-echo '[INFO] Building debug cam-server'
-echo '[WARN] Do not use in a production enviroment'
-pause
-echo '[INFO] Installing dependencies'
-pip install pyinstaller flask
-echo '[INFO] Installed pyinstaller and flask'
-echo '[INFO] Building to dist'
-pyinstaller -F cam_run.py
