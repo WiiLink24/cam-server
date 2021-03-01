32c4f73990498e9e8f9b18fde7523a9198852a19
Spotlight <spotlight@joscomputing.space>
2021-03-01 16:36:49 -0600

    Add orders/images models

---
+++ models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from cam import db
+
+
+class Orders(db.Model):
+    order_id = db.Column(db.String(8), primary_key=True, unique=True)
+    date_created = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+
+
+class Images(db.Model):
+    image_id = db.Column(db.String(8), nullable=False, primary_key=True)
+    order_id = db.Column(db.String(8), db.ForeignKey("orders.order_id"), nullable=False)
