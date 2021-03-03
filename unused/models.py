11a7bf25d0e32f85952a9340dc617efacbadfcc5
Spotlight <spotlight@joscomputing.space>
2021-03-02 20:45:08 -0600

    Remove unused

--- unused/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class Servlet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Integer(4))
-    latest_ver = db.Column(db.String(4), index=True, unique=True)
