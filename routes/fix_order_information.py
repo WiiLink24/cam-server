d593034acef9c54b969b01f826d9984782b8e450
Spotlight <spotlight@joscomputing.space>
2021-03-03 17:37:33 -0600

    Finalize checkout process
    This additionally stores the user's email and the order completion status.

--- routes/fix_order_information.py
+++ routes/fix_order_information.py
@@ -1,6 +1,53 @@
-from camlib import response
+from werkzeug import exceptions
+
+from cam import db
+from camlib import response, item_data
+from models import Orders
 
 
 @response()
-def fix_order_information(_):
-    return {}
+def fix_order_information(request):
+    order_id = request.form.get("orderID")
+    item_code = request.form.get("itemCode_1")
+    if not order_id or not item_code:
+        return exceptions.BadRequest()
+
+    current_order = Orders.query.filter_by(order_id=order_id).first()
+    if not current_order:
+        return exceptions.Forbidden()
+
+    # TODO: Start rendering of order
+    current_order.complete = True
+    db.session.commit()
+
+    # We only need a subset of the full item data.
+    current_item = item_data.get_item(item_code)
+    if not current_item:
+        return exceptions.BadRequest()
+
+    eventual_response = {
+        "available": 1,
+        "itemCode": item_code,
+        "itemPriceCode": item_code,
+        "orderID": order_id,
+        "orderDate": "today",
+        "commodityCount": [1],
+        "commodityPrice": [100000000],
+        "taxOffAmt": [210000],
+        "tax": [210000],
+        "taxInAmt": [210000],
+        "totalAmt": 0,
+        "deliveryFee": 0,
+        "daibikiFee": 0,
+        "totalTax": 0,
+        "otherFee": 0,
+        "amountTotal": 0,
+        "totalItemPrice": 0,
+        "totalShipping": 0,
+        "totalHandling": 0,
+    }
+
+    # Add our item data.
+    eventual_response |= current_item.map_list()
+
+    return eventual_response
