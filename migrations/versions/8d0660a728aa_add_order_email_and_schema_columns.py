dff1fc8a832ef91e26dcaebb6e1cc656b9992067
Spotlight <spotlight@joscomputing.space>
2021-03-03 15:41:38 -0600

    Store rendering data

--- migrations/versions/8d0660a728aa_add_order_email_and_schema_columns.py
+++ migrations/versions/8d0660a728aa_add_order_email_and_schema_columns.py
@@ -1,8 +1,8 @@
 """Add order email and schema columns
 
-Revision ID: aa613fd691a1
+Revision ID: 8d0660a728aa
 Revises: db62843cc80b
-Create Date: 2021-03-02 20:56:37.154317
+Create Date: 2021-03-03 15:40:01.170183
 
 """
 from alembic import op
@@ -10,17 +10,17 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "aa613fd691a1"
+revision = "8d0660a728aa"
 down_revision = "db62843cc80b"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column("orders", sa.Column("email", sa.String(length=127)))
-    op.add_column("orders", sa.Column("order_schema", sa.UnicodeText()))
+    op.add_column("orders", sa.Column("email", sa.String(length=127), nullable=True))
+    op.add_column("orders", sa.Column("order_schema", sa.UnicodeText(), nullable=True))
 
 
 def downgrade():
-    op.drop_column("orders", "email")
     op.drop_column("orders", "order_schema")
+    op.drop_column("orders", "email")
--- routes/get_service_information.py
+++ routes/get_service_information.py
@@ -1,6 +1,38 @@
-from camlib import response
+from werkzeug import exceptions
+
+from cam import db
+from camlib import response, item_data
+from models import Orders
 
 
 @response()
-def get_service_information(_):
-    return {}
+def get_service_information(request):
+    order_id = request.form.get("orderID")
+    item_code = request.form.get("itemCode_1")
+    service_data = request.form.get("serviceData_1")
+    if not service_data or not order_id or not item_code:
+        return exceptions.BadRequest()
+
+    # Obtain the item we are working with.
+    current_item = item_data.get_item(item_code)
+    if not current_item:
+        return exceptions.BadRequest()
+
+    # Ensure we have a valid order ID.
+    current_order = Orders.query.filter_by(order_id=order_id).first()
+    if not current_order:
+        return exceptions.Forbidden()
+
+    # Update our current order.
+    current_order.order_schema = service_data
+    db.session.commit()
+
+    # Formulate a response.
+    # First, we need to set that it was available.
+    item_response = {"available": 1}
+    # Next, merge our item's data to our response.
+    item_response |= current_item.map_list()
+    # Lastly, add a tax amount.
+    item_response["taxInAmt_1"] = "1000000"
+
+    return item_response
