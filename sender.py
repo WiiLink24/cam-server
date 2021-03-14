ecc613afe5bad553ef11a833b698cc7e11a92889
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-03-13 22:48:07 -0500

    Finish sender

--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 # Sends an image to a designated user over email
 # TODO: Address concerns about file size and whether to store them on an external server
 # and if we do, address concerns about user privacy, possibly IP locking them?
-import binascii
+import base64
 import config
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import (
@@ -15,24 +15,30 @@ from sendgrid.helpers.mail import (
 
 
 def digicam_sender(file, toemail):
+    """Sends the images to the users email"""
     msg = Mail(
-        from_email="digicam@wiilink24.com",
+        from_email="noahpistilli@gmail.com",
         to_emails=toemail,
         subject="Here is your photo!",
-        content="The photo is in attachments! Enjoy!",
+        html_content="The photo is in attachments! Enjoy!",
     )
-    handle = open(file)
-    datatosend = handle.read()
-    encoded_file = binascii.b2a_base64(datatosend, newline=True).decode()
+
+    with open(file, 'rb') as f:
+        data = f.read()
+        f.close()
+
+    encoded_file = base64.b64encode(data).decode()
+
     msg.attachment = Attachment(
         FileContent(encoded_file),
-        FileName("image.jpg"),
-        FileType("image/jpeg"),
+        FileName("images.zip"),
+        FileType("application/zip"),
         Disposition("attachment"),
     )
-    response = SendGridAPIClient(config.sendgrid_key).send(msg)
-    return (
-        response.status_code,
-        response.body,
-        response.headers,
-    )
+
+    try:
+        sg = SendGridAPIClient(config.sendgrid_key)
+        response = sg.send(msg)
+        print(response.status_code)
+    except Exception as e:
+        print(e.msg)
