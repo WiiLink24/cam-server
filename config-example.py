d242d28dbd69081e71f8b6905fbfbfb2ff172cff
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-03-13 22:49:39 -0500

    Add SendGrid API key

--- config-example.py
+++ config-example.py
@@ -1,2 +1,5 @@
 # Primary config for cam-server
 db_url = "postgresql://username:password@localhost/database_name"
+
+# Put your super secret SendGrid API Key here
+sendgrid_key = "please_change_me"
