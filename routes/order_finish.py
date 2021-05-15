0157e9e82ee9dae42abbe8f7ea0799c003ae66b2
Spotlight <spotlight@joscomputing.space>
2021-05-14 20:29:04 -0500

    Do not attempt newlines in order_finish
    Digicam just strips them, leaving a rather ominous "Congratulations!"

--- routes/order_finish.py
+++ routes/order_finish.py
@@ -7,6 +7,6 @@ def notice_order_finish(_):
         "errorItems": [0],
         "available": 0,
         "fixOrderText": "Your order has been finalized!",
-        "messageBoardText": "Congratulations!\nYour order has been dispatched via email.\n"
-        "Please wait up to 5 minutes for delivery.",
+        "messageBoardText": "Congratulations! Your order has been dispatched via email. "
+        "Please allow up to 10 minutes for delivery.",
     }
